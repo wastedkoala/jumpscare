563219ecf9e7ffbfcd94ae22316ffdf945df0964
wastedkoala <76784949+wastedkoala@users.noreply.github.com>
2025-02-08 13:02:08 +0100

    Create jumper.py

---
+++ jumper.py
@@ -0,0 +1,36 @@
+import socket, os, requests, time
+import win32gui
+import win32con
+import pyvolume
+
+hostname = '192.168.0.10'
+port = 8080
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind(hostname, port)
+
+s.listen()
+
+conn, addr = s.accept()
+
+conn.send("connectet".encode("utf-8"))
+
+while True:
+    command = conn.recv(5024).decode("utf-8")
+    if command == "jumpscare":
+        if not os.path.exists("jumpscare.mkv"):
+            url = "https://github.com/wastedkoala/jumpscare/raw/refs/heads/main/jumpscare.mkv"
+            r = requests.get(url)
+            open("jumpscare.mkv", "wb").write(r.content)
+        
+        pyvolume.custom(percent=80)
+        time.sleep(0.5)
+        os.startfile("jumpscare.mkv")
+        jump = win32gui.GetForegroundWindow()
+        win32gui.ShowWindow(jump, win32con.SW_MAXIMIZE)
+        time.sleep(2.5)
+        win32gui.PostMessage(jump,win32con.WM_CLOSE,0,0)
+
+    
+        
